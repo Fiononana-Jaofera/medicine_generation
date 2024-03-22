af1854257895f88a48497453b3f14cb15f1b8d36
Fiononana-Jaofera <fiononana.andriatsalama@gmail.com>
2024-03-22 16:59:06 +0300

    effects component

--- src/routes/consultation/consultation.component.jsx
+++ src/routes/consultation/consultation.component.jsx
@@ -1,35 +1,21 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import BasicTable from '../../components/table/table.component';
-import Button from '@mui/material/Button';
+import Form from '../../components/form/form.component';
 
 const Consultation = () => {
     const effects = ['e1', 'e2'];
     return (
         <div>
-            <Box
-                component="form"
-                sx={{
-                    '& > :not(style)': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                <h2>Maladies :</h2>
-                {effects.map((e, id) => <TextField
-                    id="outlined-basic"
-                    type="number"
-                    key={id}
-                    label={e}
-                    variant="outlined" />)}
-                <Button variant='contained' type='submit' color='success'>Consulter</Button>
-                
-            </Box>
+            <Form
+                labels={effects}
+                title={"Maladies"}
+                button={{
+                    color: 'success',
+                    text: 'Consulter'
+                }} />
             <h2>Résultat :</h2>
             <BasicTable
                 headers={['Médicaments', 'Prix Unitaire', 'Quantité', 'Total']}
-                body={[['m1', 100, 3, 300],['m2', 100, 1, 100], ['','','', 400]]} />
+                body={[['m1', 100, 3, 300], ['m2', 100, 1, 100], ['', '', '', 400]]} />
         </div>
     );
 }
